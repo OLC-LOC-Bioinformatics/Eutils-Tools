fb73a0660fb5f6fc8abe7359d77e02c38eb8cbc2
mike <mikewknowles@gmail.com>
2015-12-23 13:58:21 -0500

    fixed default

--- WGS-fetch.py
+++ WGS-fetch.py
@@ -29,66 +29,67 @@ def parser(dqueue):
     :param dqueue: Queue for multiple downloads at once
     :return: fasta WGS or complete chromosome
     """
-    try:  # this is a loop to help multithreading
-        gi, count, path = dqueue.get()  # retrieve tuple from the queue
-        connected = False  # Start not connected
-        while not connected:  # Loop to overcome connection issues with a terrible network
-            try:
-                fetch = Entrez.efetch(db="nuccore",
-                                      id=gi,
-                                      rettype='fasta',
-                                      retmode='text')
-                connected = True
-            except IndexError:
-                print "[%s] Unable to download" % (time.strftime("%H:%M:%S"))
-                connected = True
-            except ValueError:
-                print "[%s] No records found in handle" % (time.strftime("%H:%M:%S"))
-                connected = True
-            except HTTPError:
-                print "[%s] Unable to connect trying again in 5 seconds" % (time.strftime("%H:%M:%S"))
-                time.sleep(5)
-        record = SeqIO.read(fetch, "fasta")  # reads the fasta data from NCBI
-        fetch.close()  # Good practise to close unused handles
-        ecoli = record.description.replace(record.id, "")[1:].split(",")[0]  # string split
-        print "[%s] Processing %s GI:%s" % (time.strftime("%H:%M:%S"), ecoli, gi)
-        filename = sub('[^A-Za-z0-9]+str[ain.]*[^A-Za-z0-9]*|[^A-Za-z0-9]+subsp\.*[^A-Za-z0-9]*'
-                       '|[^A-Za-z0-9]+serovar\.*[^A-Za-z0-9]*|[^A-Za-z0-9]+', '_', ecoli)
-        name = filename.replace("_complete_genome", "")
-        # Create filename for a cataloging and removing unneccesary bits
-        filename = path + name + ".fasta"
-        fasta = open(filename, 'w')
-        if "_" in record.id:
-            urlid = record.id.split("_")[-1]
-        else:
-            urlid = record.id
-        # faster method to retrive genomes
-        url = "https://www.ncbi.nlm.nih.gov/Traces/wgs/?download=" + urlid[:5] + "1.1.fsa_nt.gz"
+    while True:  # this is a loop to help multithreading
         try:
-            source = urllib.urlopen(url).read()
-            compressedFile = StringIO()
-            compressedFile.write(source)
-            #
-            # Set the file's current position to the beginning
-            # of the file so that gzip.GzipFile can read
-            # its contents from the top.
-            #
-            compressedFile.seek(0)
+            gi, count, path = dqueue.get()  # retrieve tuple from the queue
+            connected = False  # Start not connected
+            while not connected:  # Loop to overcome connection issues with a terrible network
+                try:
+                    fetch = Entrez.efetch(db="nuccore",
+                                          id=gi,
+                                          rettype='fasta',
+                                          retmode='text')
+                    connected = True
+                except IndexError:
+                    print "[%s] Unable to download" % (time.strftime("%H:%M:%S"))
+                    connected = True
+                except ValueError:
+                    print "[%s] No records found in handle" % (time.strftime("%H:%M:%S"))
+                    connected = True
+                except HTTPError:
+                    print "[%s] Unable to connect trying again in 5 seconds" % (time.strftime("%H:%M:%S"))
+                    time.sleep(5)
+            record = SeqIO.read(fetch, "fasta")  # reads the fasta data from NCBI
+            fetch.close()  # Good practise to close unused handles
+            ecoli = record.description.replace(record.id, "")[1:].split(",")[0]  # string split
+            print "[%s] Processing %s GI:%s" % (time.strftime("%H:%M:%S"), ecoli, gi)
+            filename = sub('[^A-Za-z0-9]+str[ain.]*[^A-Za-z0-9]*|[^A-Za-z0-9]+subsp\.*[^A-Za-z0-9]*'
+                           '|[^A-Za-z0-9]+serovar\.*[^A-Za-z0-9]*|[^A-Za-z0-9]+', '_', ecoli)
+            name = filename.replace("_complete_genome", "")
+            # Create filename for a cataloging and removing unneccesary bits
+            filename = path + name + ".fasta"
+            fasta = open(filename, 'w')
+            if "_" in record.id:
+                urlid = record.id.split("_")[-1]
+            else:
+                urlid = record.id
+            # faster method to retrive genomes
+            url = "https://www.ncbi.nlm.nih.gov/Traces/wgs/?download=" + urlid[:5] + "1.1.fsa_nt.gz"
+            try:
+                source = urllib.urlopen(url).read()
+                compressedFile = StringIO()
+                compressedFile.write(source)
+                #
+                # Set the file's current position to the beginning
+                # of the file so that gzip.GzipFile can read
+                # its contents from the top.
+                #
+                compressedFile.seek(0)
 
-            decompressedFile = GzipFile(fileobj=compressedFile, mode='rb')
-            # with open(filename, 'w') as outfile:
-            #     outfile.write(decompressedFile.read())
+                decompressedFile = GzipFile(fileobj=compressedFile, mode='rb')
+                # with open(filename, 'w') as outfile:
+                #     outfile.write(decompressedFile.read())
 
-            fasta.write(decompressedFile.read())
-            print "[%s] Downloading and unzipping #%i %s..." % (time.strftime("%H:%M:%S"), count, name)
-        except IOError:
-            fasta = open(filename, "w")
-            print "[%s] Downloading #%i %s..." % (time.strftime("%H:%M:%S"), count, name)
-            SeqIO.write(record, fasta, "fasta")
-        fasta.close()
-    except KeyboardInterrupt:
-        raise KeyboardInterruptError()
-    dqueue.task_done()
+                fasta.write(decompressedFile.read())
+                print "[%s] Downloading and unzipping #%i %s..." % (time.strftime("%H:%M:%S"), count, name)
+            except IOError:
+                fasta = open(filename, "w")
+                print "[%s] Downloading #%i %s..." % (time.strftime("%H:%M:%S"), count, name)
+                SeqIO.write(record, fasta, "fasta")
+            fasta.close()
+        except KeyboardInterrupt:
+            raise KeyboardInterruptError()
+        dqueue.task_done()
 
 
 def dlthreads(email, organism, path, length, arg=''):
